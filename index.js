7e014aebe21737d0add2677a7e158d66853108c1
Kashish soni <159767573+kashishsoni004@users.noreply.github.com>
2026-02-08 22:41:10 +0530

    Update server response message and log output

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var http = require('http');
 
-//create a server object:
+// Create a server object:
 http.createServer(function (req, res) {
-  res.write('Welcome to DevOps Training'); //write a response to the client
-  res.end(); //end the response
-}).listen(3000); //the server object listens on port 80
+  res.write('Hello Docker container! 🚀');  // write a response to the client
+  res.end(); // end the response
+}).listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
